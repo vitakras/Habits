407e46174f98a83e8bfe39a2694ebe556bf1ab20
Vitaliy Krasylovets <Vitaliy@Vitaliys-MacBook-Pro.local>
2015-06-21 22:14:45 -0400

    Refactor MainActivity

--- app/src/main/java/com/example/vitaliy/habits/Activities/MainActivity.java
+++ app/src/main/java/com/example/vitaliy/habits/Activities/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.vitaliy.habits.Activities;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import com.example.vitaliy.habits.R;
+import android.support.v4.app.FragmentTransaction;
 
+import com.example.vitaliy.habits.Fragments.CreateHabitFragment;
+import com.example.vitaliy.habits.R;
 
 public class MainActivity extends FragmentActivity {
 
@@ -11,5 +13,25 @@ public class MainActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Make sure fragment container exists
+        if (findViewById(R.id.fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create new fragment to be placed in the activity
+            CreateHabitFragment firstFragment = new CreateHabitFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            transaction.add(R.id.fragment_container, firstFragment);
+
+            transaction.commit();
+        }
     }
 }
--- app/src/main/java/com/example/vitaliy/habits/Fragments/CreateHabitFragment.java
+++ app/src/main/java/com/example/vitaliy/habits/Fragments/CreateHabitFragment.java
@@ -1,7 +1,7 @@
 package com.example.vitaliy.habits.Fragments;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
