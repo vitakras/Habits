ec8b2dde90eec281082a87e705a303e0d5199b45
Vitaliy Krasylovets <Vitaliy@Vitaliys-MacBook-Pro.local>
2015-06-28 20:05:30 -0400

    Added table column info for alarm

--- app/src/main/java/com/example/vitaliy/habits/DB/HabitsContract.java
+++ app/src/main/java/com/example/vitaliy/habits/DB/HabitsContract.java
@@ -14,4 +14,19 @@ public final class HabitsContract {
         public static final String COLUMN_NAME_NAME = "name";
         public static final String COLUMN_NAME_SUMMARY = "summary";
     }
+
+    public static abstract class AlarmColumns implements BaseColumns {
+        public static final String TABLE_NAME = "alarm";
+        public static final String COLUMN_HOUR = "hour";
+        public static final String COLUMN_MINUTE = "minute";
+        public static final String COLUMN_REPEAT_WEEKLY = "repeatWeekly";
+        public static final String COLUMN_ENABLED = "enabled";
+        public static final String COLUMN_TONE = "tone";
+    }
+
+    public static abstract class RepeatingDayColumns implements BaseColumns {
+        public static final String TABLE_NAME = "repeatingDay";
+        public static final String COLUMN_ALARM_ID = "alarmID";
+        public static final String COLUMN_DAY = "day";
+    }
 }
