7412da218e8a3110419441989b5fbd79821e6de9
Raahul Biswas <raahul.biswas@ryerson.ca>
2015-07-07 20:09:14 -0400

    adjusted some margins, changed primary and secondary colours to match material design guidelines

--- app/src/main/java/com/example/vitaliy/habits/DB/DataBaseHelper.java
+++ app/src/main/java/com/example/vitaliy/habits/DB/DataBaseHelper.java
@@ -18,6 +18,7 @@ import java.util.List;
  */
 public class DatabaseHelper extends SQLiteOpenHelper implements IDatabaseHabits {
 
+
     // Logcat tag
     private static final String LOG = DatabaseHelper.class.getName();
 
